2c0bece37a3e3cf34dcd96a27d4c5b4c055a35d0
3sfoor102 <3sfoor102@gmail.com>
2026-06-25 04:17:01 +0300

    Added variables and created a loop to compare words

--- app.js
+++ app.js
@@ -8,11 +8,10 @@
 // I need to create a list of words, to compare the
 // current word the user trying to guess and the actual word
 let wordList = ['apple', 'orange', 'banana', 'mango', 'watermelon', 'grape', 'cherry', 'Strawberry']
-let guessedWord = [] //maybe I need to define it inside a specific function to avoid saving globally
+let guessedWord = ['a','p','p','l','e'] // maybe I need to define it inside a specific function to avoid saving globally
+let savedWord = [randomWord(wordList)] // this variable saves the result of the randomWord() and it saves it specifically in an array, so it can be compared later on
+console.log(savedWord)
 
-// for (let i = 0; let i<)
-
-//console.log(wordList[randomIndex(wordList)])
 
 
  
@@ -36,7 +35,19 @@ function randomWord(listName){
 // after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
 // it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 }
+// console.log(randomWord(wordList))
 
-console.log(randomWord(wordList))
 
-console.log(wordList[3])
+
+// I made a crucial logical error, where I wanted to check if the gussedWord array content is identical to the content of a specific random word, if it is I want it to print the letters of the gussedWord, however here I have added the randomWord function inside the loop itself, so, assuming the random word at first iteration is "apple" in the second iteration when the loop runs again the word will be already changed, since it is not saved, it is just calling the function each time I run the loop, for example, it might pick another word from the array list like "mango"
+for (let i = 0; i<wordList.length; i++){
+    for (let j=0; j<wordList[i].length; j++)
+    {
+        if (guessedWord[i] === savedWord[i]){
+            // console.log(`Gussed letter is: ${guessedWord[i]}`)
+        }
+        // console.log(wordList[i][j])
+        // guessedWord[i].push(j)
+    }
+}
+
