1b2ad2e2740f4fdfc09ffd49da94f1cbb268fc68
3sfoor102 <3sfoor102@gmail.com>
2026-06-29 15:48:45 +0300

    updated createWordOutput with condition

--- app.js
+++ app.js
@@ -30,19 +30,19 @@ const liEls = document.createElement('li')
 // We use forEach to iterate in all the letters that are in "letters" class
 // Then we add an event listner to see and save which letter the user have clicked
 
- 
-lettersBtns.forEach(function(button){
-button.addEventListener('click', function(event){
-    // this for loop go over the selected word, the randoWord,
-    // and it goes over each letter in it, to compare it with letters
-    // entered by the user
-    // for (let i = 0; i < secretWord.length; i++) {
-    //     for (let j = 0; j < secretWord[i].length; j++) {
-    //          console.log(secretWord[i][j])
-    //     }
-    //     // if (pressedKey[0] === secretWord[0][])
-    //     // guessedWord.push(button.textContent)
-    // }
+
+lettersBtns.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        // this for loop go over the selected word, the randoWord,
+        // and it goes over each letter in it, to compare it with letters
+        // entered by the user
+        // for (let i = 0; i < secretWord.length; i++) {
+        //     for (let j = 0; j < secretWord[i].length; j++) {
+        //          console.log(secretWord[i][j])
+        //     }
+        //     // if (pressedKey[0] === secretWord[0][])
+        //     // guessedWord.push(button.textContent)
+        // }
 
     })
 })
@@ -60,40 +60,49 @@ button.addEventListener('click', function(event){
 // Where I only need to add the list name 
 // and the function will give me a random item from the list,
 // which is a word
-function randomWord(listName){
+function randomWord(listName) {
     let randomIndex = Math.floor(Math.random() * listName.length)
     return listName[randomIndex]
-// the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
-// after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
-// it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
+    // the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
+    // after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
+    // it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 }
 // console.log(randomWord(wordList))
-function createWordOutput () {
-    seprateSeceretLetters()
+seprateSeceretLetters()
+function createWordOutput() {
     for (let i = 0; i < secretWordLettersArray.length; i++) {
         ulDashesEl.append(liEls.textContent = '___ ')
     }
-
-    lettersBtns.forEach(function(button){
-    button.addEventListener('click', function(event){
-        ulLettersEl.append(liEls.textContent = event.target.textContent)
+    // # I should place seprateSeceretLetters() --> Invoke, then
+    // # I should invoke createWordOutput(), because first I will know the letters, then I can compare them
+    // # Each letter should be placed at the same location of its location in the original array
+    // # If the letter is 'l', and the seceretWord is  apple, the letter should
+    // # be placed at index 3, and it showed be shown at index 3 in the screen 
+    lettersBtns.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            if (event.target.textContent === secretWordLettersArray[0]) {
+                ulLettersEl.append(liEls.textContent = event.target.textContent + ' ')
+                console.log('GOOD')
+            }
+        })
     })
-})
 
 }
-console.log(createWordOutput())
+createWordOutput()
+
+
 
 // seprateSeceretLetters is used to seperate the full random word
 // and it saves each letter in it  
-function seprateSeceretLetters () {
+function seprateSeceretLetters() {
     for (let i = 0; i < secretWord.length; i++) {
-    for (let j = 0; j < secretWord[i].length; j++) {
-        secretWordLettersArray.push(secretWord[i][j])
+        for (let j = 0; j < secretWord[i].length; j++) {
+            secretWordLettersArray.push(secretWord[i][j])
         }
     }
 }
-    // console.log(secretWordLettersArray)
-    console.log(secretWordLettersArray.length)
+console.log(secretWordLettersArray)
+// console.log(secretWordLettersArray.length)
 
 
 
