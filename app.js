4114df29e5957e4c5ae3c6f38827ec192deb0599
3sfoor102 <3sfoor102@gmail.com>
2026-06-30 12:09:02 +0300

    Cleaned the code structure

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 /*-------------------------------- Pseudocode --------------------------------*/
+// Comparing Words Logic 
+    // Each letter should be placed at the same location of its location in the original array
+    // If the letter is 'l', and the seceretWord is  apple, the letter should
+    // be placed at index 3, and it showed be shown at index 3 in the screen
+
+// lives (attempts) and Images     
+    // Add one image, and this image changes according to status, for example the first image with full body, the second image will replace the first image but with hand lost, etc.
+    // Each wrong letter pressed will take a life from the user, using lives--
 
 
 
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-// I need to create a list of words, to compare the
-// current word the user trying to guess and the actual word
 let wordList = ['apple', 'orange', 'banana', 'mango', 'watermelon', 'grape', 'cherry', 'Strawberry']
-let guessedWord = [] // maybe I need to define it inside a specific function to avoid saving globally
-let secretWord = [randomWord(wordList)] //[randomWord(wordList)] // this variable saves the result of the randomWord() and it saves it specifically in an array, so it can be compared later on
+let guessedWord = [] 
+let secretWord = [randomWord(wordList)] 
+let secretWordLettersArray = []
 let pressedKey = ['a']
 let correctLetter = false
 let lives = 6
 let playing = false
 let winner = false
-let secretWordLettersArray = []
 
 
 
@@ -25,53 +31,28 @@ const ulDashesEl = document.querySelector('.output-dashes')
 const ulLettersEl = document.querySelector('.output-letters')
 const liEls = document.createElement('li')
 const imgEls = document.querySelector('.images')
-// imgEls.classList.replace('images', 'removeImage')
-
-console.log(secretWord)
-// This brings the keyboard letters from the html page
-// We use forEach to iterate in all the letters that are in "letters" class
-// Then we add an event listner to see and save which letter the user have clicked
-
-
-lettersBtns.forEach(function (button) {
-    button.addEventListener('click', function (event) {
-        // this for loop go over the selected word, the randoWord,
-        // and it goes over each letter in it, to compare it with letters
-        // entered by the user
-        // for (let i = 0; i < secretWord.length; i++) {
-        //     for (let j = 0; j < secretWord[i].length; j++) {
-        //          console.log(secretWord[i][j])
-        //     }
-        //     // if (pressedKey[0] === secretWord[0][])
-        //     // guessedWord.push(button.textContent)
-        // }
-
-    })
-})
-
-
+const divEl = document.querySelector('div')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-/*-------------------------------- Functions --------------------------------*/
-// The randomWord function is simply picking one random words from the array list,
-// which will be usefuel if I decided to use multiple types of lists,
-// e.g. food list, cars, countries, etc.
-// Where I only need to add the list name 
-// and the function will give me a random item from the list,
-// which is a word
-        console.log(secretWordLettersArray)
 
+
+/*-------------------------------- Functions --------------------------------*/
 function randomWord(listName) {
     let randomIndex = Math.floor(Math.random() * listName.length)
     return listName[randomIndex]
-    // the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
-    // after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
-    // it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 }
-seprateSeceretLetters()
-function createWordOutput() {
+
+function seprateSeceretLetters() {
+    for (let i = 0; i < secretWord.length; i++) {
+        for (let j = 0; j < secretWord[i].length; j++) {
+            secretWordLettersArray.push(secretWord[i][j])
+        }
+    }
+    }
+
+function createWordOutput () {
     for (let i = 0; i < secretWordLettersArray.length; i++) {
         ulDashesEl.append(liEls.textContent = '___ ')
     }
@@ -86,59 +67,59 @@ function createWordOutput() {
 
         })
     })
-
 }
+
+
+// =============================== CONSOLE LOGS AND CALLING FUNCTIONS =====================================
+seprateSeceretLetters() 
 createWordOutput()
-
-
-
-// seprateSeceretLetters is used to seperate the full random word
-// and it saves each letter in it  
-function seprateSeceretLetters() {
-    for (let i = 0; i < secretWord.length; i++) {
-        for (let j = 0; j < secretWord[i].length; j++) {
-            secretWordLettersArray.push(secretWord[i][j])
-        }
-    }
-}
 console.log(secretWordLettersArray)
-// console.log(secretWordLettersArray.length)
 
+// =======================================================================================
 
-
+// =============================== CODES I MIGHT USE =====================================
 // this comparision is working, just replace userInput with the currenyUserLetter 
-// for (let i = 0; i < secretWordLettersArray.length; i++) {
-//         if (secretWordLettersArray[i] === userinput[i]) {
-//         console.log('true')
+    // for (let i = 0; i < secretWordLettersArray.length; i++) {
+    //         if (secretWordLettersArray[i] === userinput[i]) {
+    //         console.log('true')
 
-//         }
-//     }
+    //         }
+    //     }
 
+// Disabling a button
+    // const testDisableButton = document.querySelector('#A')
+    // console.log(testDisableButton)
+    // testDisableButton.disabled = true
 
+// Replacing a class     
+    // imgEls.classList.replace('images', 'removeImage')
 
-// =============================== PSEUDOCODE START =====================================
-// I should place seprateSeceretLetters() --> Invoke, then
-// I should invoke createWordOutput(), because first I will know the letters, then I can compare them
-// Each letter should be placed at the same location of its location in the original array
-// If the letter is 'l', and the seceretWord is  apple, the letter should
-// be placed at index 3, and it showed be shown at index 3 in the screen
-
-// Add one image, and this image changes according to status, for example the first image with full body, the second image will replace the first image but with hand lost, etc.
-// 
+// ==========================================================================================
 
 
-// =============================== PSEUDOCODE END =====================================
+// =============================== MAIN CODE EXPLAINED:  =====================================
+// seprateSeceretLetters function 
+    // seprateSeceretLetters function is used to seperate the full random word and it saves each letter in it  
 
+// randomWord function
+    // The randomWord function is simply picking one random words from the array list,
+    // which will be usefuel if I decided to use multiple types of lists,
+    // e.g. food list, cars, countries, etc.
+    // Where I only need to add the list name 
+    // and the function will give me a random item from the list,
+    // which is a word
+    // the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
+    // after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
+    // it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 
-
-
+// ==========================================================================================
 
 
 // =============================== DEAD CODE =====================================
 // // WORKING 
 // lettersBtns.forEach(function(button){
 // button.addEventListener('click', function(event){
-//         // console.log(bevent.target.textContent)
+//         // console.log(event.target.textContent)
 //         guessedWord.push(button.textContent)
 //         // if (pressedKey[0] === secretWord[0][0]) {
 //         //     pressedKey.push(event.target.textContent)
@@ -156,6 +137,22 @@ console.log(secretWordLettersArray)
 // lettersBtns.forEach(function(button){
 // button.addEventListener('click', function(event){
 //     ulLettersEl.append(liEls.textContent = event.target.textContent)
+//     })
+// })
+
+// lettersBtns.forEach(function (button) {
+//     button.addEventListener('click', function (event) {
+//         // this for loop go over the selected word, the randoWord,
+//         // and it goes over each letter in it, to compare it with letters
+//         // entered by the user
+//         // for (let i = 0; i < secretWord.length; i++) {
+//         //     for (let j = 0; j < secretWord[i].length; j++) {
+//         //          console.log(secretWord[i][j])
+//         //     }
+//         //     // if (pressedKey[0] === secretWord[0][])
+//         //     // guessedWord.push(button.textContent)
+//         // }
+
 //     })
 // })
 
