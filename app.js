640fce1ddb0210d4f8b43f35025e71b592269ad3
3sfoor102 <3sfoor102@gmail.com>
2026-07-01 00:21:42 +0300

    win logic and function is ready

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 /*-------------------------------- Pseudocode --------------------------------*/
 // Comparing Words Logic 
-    // Each letter should be placed at the same location of its original location in the secretWordLettersArray
-    // If the letter is 'l', and the seceretWord is  apple, the letter should
-    // be placed at index 3, and it showed be shown at index 3 in the screen
+// Each letter should be placed at the same location of its original location in the secretWordLettersArray
+// If the letter is 'l', and the seceretWord is  apple, the letter should
+// be placed at index 3, and it showed be shown at index 3 in the screen
 
 // lives (attempts) and Images     
-    // Add one image, and this image changes according to status, for example the first image with full body, the second image will replace the first image but with hand lost, etc.
-    // Each wrong letter pressed will take a life from the user, using lives--
+// Add one image, and this image changes according to status, for example the first image with full body, the second image will replace the first image but with hand lost, etc.
+// Each wrong letter pressed will take a life from the user, using lives--
 
 // Win 
-    // if the user gussedWord === seceretWord && attempts 
+// if the user gussedWord === seceretWord && attempts 
 
 // Reset 
-    // add the random word to it secretWord = [randomWord(wordList)] 
-    // Reset all keyboard letters
-    // Reset all the spaces
-    // Reset the image to the first image, or none image
-    // Reset music and all sounds if needed
+// add the random word to it secretWord = [randomWord(wordList)] 
+// Reset all keyboard letters
+// Reset all the spaces
+// Reset the image to the first image, or none image
+// Reset music and all sounds if needed
 
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
-let wordList = ['apple', 'orange', 'banana', 'mango', 'watermelon', 'grape', 'cherry', 'Strawberry']
-let secretWord = [randomWord(wordList)] 
+let wordList = ['apple', 'orange', 'banana', 'mango', 'watermelon', 'grape', 'cherry', 'strawberry']
+let secretWord = [randomWord(wordList)]
+let gussedWord = []
 let secretWordLettersArray = []
 let correctLetter = false
 let playing = false
@@ -33,9 +34,9 @@ let lives = 6
 
 /*------------------------ Cached Element References ------------------------*/
 const lettersBtns = document.querySelectorAll('.letters')
-const ulDashesEl = document.querySelector('.output-dashes')
-const ulLettersEl = document.querySelector('.output-letters')
-const liEls = document.createElement('li')
+// const ulDashesEl = document.querySelector('.output-dashes')
+// const ulLettersEl = document.querySelector('.output-letters')
+// const liEls = document.createElement('li')
 const imgEls = document.querySelector('.images')
 const divsContainerEl = document.querySelector('.divs-container')
 const resetEl = document.querySelector('#reset-btn')
@@ -58,9 +59,9 @@ function seprateSeceretLetters() {
             secretWordLettersArray.push(secretWord[i][j])
         }
     }
-    }
+}
 
-function createWordOutput () {
+function createWordOutput() {
     seprateSeceretLetters()
     for (let i = 0; i < secretWordLettersArray.length; i++) {
         const divEl = document.createElement('div')
@@ -68,41 +69,50 @@ function createWordOutput () {
         divEl.id = [i]
         divEl.classList = 'words-spaces'
     }
+
     divsContainerEls = document.querySelectorAll('.words-spaces')
+
     lettersBtns.forEach(function (button) {
         button.addEventListener('click', function (event) {
             for (let i = 0; i < secretWordLettersArray.length; i++) {
                 if (event.target.textContent === secretWordLettersArray[i]) {
+
                     // if the letter is correct and not in the "gussed word list at that specific index it will add it their"
                     // where div element id is the same as the pushed letter (for example div id [1] === seceretWord [1] ... it should at the seceret word at 1 at the same place )
                     const pEl = document.createElement('p')
                     pEl.textContent = event.target.textContent
                     divsContainerEls[i].appendChild(pEl)
-
+                    // here, the logic already prevent the user from inserting a wrong letter, so any inserted letter is correct, all of the inserted letters are added to the gussedWord array, without any index arrangment, however, since all the inserted letters are 100% correct this means the length of the gussedWord will be = to the length of the seceret word, so if the length is equal I can know that the user have placed all the correct letters
+                    gussedWord.push(event.target.textContent)
+                    console.log(`gussedWord ${gussedWord.length}`)
+                    console.log(`secert length ${secretWordLettersArray.length}`)
                     const DisableButton = event.target
                     DisableButton.disabled = true
-                                        
+                    winCheck()
+
 
                 } else if (event.target.textContent !== secretWordLettersArray[i]) {
                     console.log(lives)
                     DisableButton = event.target
                     DisableButton.disabled = true
                     // lives--
-                    
-                } 
+
+                }
 
             }
-        }) 
+        })
     })
 
 }
 
-function winCheck () {
-   
-        // lives >=1
+function winCheck() {
+
+    if (gussedWord.length === secretWordLettersArray.length) {
+        console.log('You Won!')
+
+    }
 }
-winCheck()
-function reset () {
+function reset() {
     console.log('HELLPW')
     secretWord = [randomWord(wordList)]
     createWordOutput()
@@ -114,23 +124,22 @@ function reset () {
 
 // =============================== CONSOLE LOGS AND CALLING FUNCTIONS =====================================
 createWordOutput()
-// console.log(divsContainerEls)
-console.log(secretWordLettersArray)
+console.log('this is secretWordLettersArray ----- ' + secretWordLettersArray)
 resetEl.addEventListener('click', reset)
-console.log(secretWord)
-console.log()
+console.log('This is secret word ----- ' + secretWord)
+
 
 
 // =======================================================================================
 
 // =============================== CODES I MIGHT USE =====================================
 // this comparision is working, just replace userInput with the currenyUserLetter 
-    // for (let i = 0; i < secretWordLettersArray.length; i++) {
-    //         if (secretWordLettersArray[i] === userinput[i]) {
-    //         console.log('true')
+// for (let i = 0; i < secretWordLettersArray.length; i++) {
+//         if (secretWordLettersArray[i] === userinput[i]) {
+//         console.log('true')
 
-    //         }
-    //     }
+//         }
+//     }
 
 // // Disabling a button
 //     const testDisableButton = document.querySelector('#A')
@@ -138,25 +147,25 @@ console.log()
 //     testDisableButton.disabled = true
 
 // Replacing a class     
-    // imgEls.classList.replace('images', 'removeImage')
+// imgEls.classList.replace('images', 'removeImage')
 
 // ==========================================================================================
 
 
 // =============================== MAIN CODE EXPLAINED:  =====================================
 // seprateSeceretLetters function 
-    // seprateSeceretLetters function is used to seperate the full random word and it saves each letter in it  
+// seprateSeceretLetters function is used to seperate the full random word and it saves each letter in it
 
 // randomWord function
-    // The randomWord function is simply picking one random words from the array list,
-    // which will be usefuel if I decided to use multiple types of lists,
-    // e.g. food list, cars, countries, etc.
-    // Where I only need to add the list name 
-    // and the function will give me a random item from the list,
-    // which is a word
-    // the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
-    // after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
-    // it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
+// The randomWord function is simply picking one random words from the array list,
+// which will be usefuel if I decided to use multiple types of lists,
+// e.g. food list, cars, countries, etc.
+// Where I only need to add the list name
+// and the function will give me a random item from the list,
+// which is a word
+// the randomIndex variables saves a radnom index, which will be used to pick a random number for the word's list
+// after generating the random number and saving it inside "randomIndex" variable we are just using it with the array to pick a random word
+// it is like we are typing wordList[0] (for example the result is "Mango"), instead of a fixed number now the random function will change this to different numbers for example (wordList[2], which will give "banana")
 
 // ==========================================================================================
 
